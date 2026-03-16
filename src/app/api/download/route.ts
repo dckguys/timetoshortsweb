b9ff3d8b361ab533013f115d38c412aaba2fa211
dydtjq94 <ghdtlr90@gmail.com>
2026-03-16 17:33:47 +0900

    pdf

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -3,13 +3,17 @@ import { readFile } from "fs/promises";
 import path from "path";
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), "public", "service.pdf");
+  const filePath = path.join(
+    process.cwd(),
+    "public",
+    "timetoshorts_service.pdf",
+  );
   const file = await readFile(filePath);
 
   return new NextResponse(file, {
     headers: {
       "Content-Type": "application/pdf",
-      "Content-Disposition": 'attachment; filename="service.pdf"',
+      "Content-Disposition": 'attachment; filename="timetoshorts_service.pdf"',
     },
   });
 }
